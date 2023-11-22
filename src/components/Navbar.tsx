7b4c32de6df7ed6dfcb3b2e19d40de826f205bc2
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2023-11-22 21:16:43 +0000

    Add hamburger menu for mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,55 @@
 import Link from "next/link";
-import { type FC } from "react";
+import { useState, type FC } from "react";
 import { sections } from "../data/sections";
 import { motion } from "framer-motion";
 import clsx from "clsx";
+import { HiMenu } from "react-icons/hi";
 import { useActiveSectionContext } from "~/context/ActiveSectionContext";
 
 const Navbar: FC = () => {
   const { activeSection, setActiveSection, setTimeOfLastClick } =
     useActiveSectionContext();
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
 
   return (
-    <nav className="fixed top-0 z-[999] hidden w-screen justify-center px-64 py-4 text-white sm:flex">
+    <nav className="fixed top-0 z-[999] flex w-screen justify-center py-4 text-white sm:px-64">
+      <div className="block sm:hidden">
+        <button
+          type="button"
+          className="text-white-900 inline-flex items-center gap-x-1 text-sm font-semibold leading-6"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          <HiMenu className="text-3xl" />
+        </button>
+        {isMenuOpen && (
+          <div className="absolute left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4">
+            <div className="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
+              <div className="p-4">
+                {sections.map((section) => (
+                  <Link
+                    key={section.hash}
+                    className="group relative flex gap-x-6 rounded-lg p-4  hover:bg-gray-50"
+                    href={section.hash}
+                    onClick={() => {
+                      setActiveSection(section.name);
+                      setTimeOfLastClick(Date.now());
+                      setIsMenuOpen(false);
+                    }}
+                  >
+                    <div className="w-full text-center text-lg  font-semibold text-gray-900">
+                      {section.name}
+                      <span className="absolute inset-0"></span>
+                    </div>
+                  </Link>
+                ))}
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+
       <motion.ul
-        className="fixed flex select-none gap-2 rounded-full bg-[#1A1A1A] bg-opacity-80 p-2 text-center font-medium backdrop-blur-[0.5rem]"
+        className="fixed hidden select-none gap-2 rounded-full bg-[#1A1A1A] bg-opacity-80 p-2 text-center font-medium backdrop-blur-[0.5rem] sm:flex"
         initial={{ y: -100, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{
@@ -31,7 +68,7 @@ const Navbar: FC = () => {
           >
             <Link
               className={clsx(
-                "flex w-full items-center justify-center px-3 py-1 transition hover:text-white ",
+                "flex w-full items-center justify-center px-3 py-1 transition ",
                 {
                   "z-10 text-black hover:text-black":
                     activeSection === section.name,
@@ -59,37 +96,6 @@ const Navbar: FC = () => {
             </Link>
           </motion.li>
         ))}
-        {/*sections.map((section) => (
-          <motion.li
-            key={section.hash}
-            initial={{ y: -100, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{
-              type: "spring",
-              stiffness: 380,
-              damping: 30,
-              delay: 0.5,
-            }}
-          >
-            <Link
-              className={clsx(
-                "rounded-full px-2 py-1 transition hover:text-gray-500",
-                {
-                  "bg-white text-black hover:text-black":
-                    activeSection === section.name,
-                },
-              )}
-              href={section.hash}
-              onClick={() => {
-                setActiveSection(section.name);
-                setTimeOfLastClick(Date.now());
-              }}
-              draggable={false}
-            >
-              {section.name}
-            </Link>
-          </motion.li>
-            ))*/}
       </motion.ul>
     </nav>
   );
