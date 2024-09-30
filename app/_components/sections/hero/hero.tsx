b2e6128e371ef5f2fb4b5c4d7b1a6aada670ff39
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2024-09-30 20:41:45 +0100

    Implement slight gradient to main title

--- app/_components/sections/hero/hero.tsx
+++ app/_components/sections/hero/hero.tsx
@@ -1,7 +1,6 @@
 "use server";
 
 import { getHeroData } from "@/src/data/home/hero";
-import { getTranslations } from "next-intl/server";
 
 import { HeroButtons } from "./hero-buttons";
 
@@ -18,7 +17,7 @@ export async function Hero() {
       className="relative flex flex-col items-center justify-center space-y-2 bg-zinc-50 py-32 bg-grid-black/[0.1] dark:bg-black dark:bg-grid-white/[0.15] sm:space-y-6 sm:py-64"
     >
       <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-zinc-50 [mask-image:radial-gradient(ellipse_at_center,transparent_35%,black)] dark:bg-black sm:[mask-image:radial-gradient(ellipse_at_center,transparent_10%,black)]"></div>
-      <h1 className="z-10 text-center text-5xl font-bold text-black dark:text-white sm:text-7xl md:text-9xl">
+      <h1 className="black-gradient z-10 text-center text-5xl font-bold sm:text-7xl md:text-9xl">
         {data.title}
       </h1>
       <p className="text-color z-10 !mb-4 text-center text-xl sm:text-2xl md:text-3xl">
