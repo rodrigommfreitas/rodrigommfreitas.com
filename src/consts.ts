23c7c19f33b0667136a176e17d2b93e4484a307f
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2025-01-18 15:50:22 +0000

    Update meta description

--- src/consts.ts
+++ src/consts.ts
@@ -5,6 +5,6 @@ export const SITE_TITLE: string | Multilingual = {
   pt: "Rodrigo Freitas - Criação de Sites Profissionais",
 };
 export const SITE_DESCRIPTION: string | Multilingual = {
-  en: "Create your professional website with Rodrigo Freitas. Boost your online presence and reach more customers.",
-  pt: "Crie seu site profissional com Rodrigo Freitas. Aumente a sua presença online e alcance mais clientes.",
+  en: "Boost your online presence and reach more customers with a professional website tailored to your needs, created by Rodrigo Freitas.",
+  pt: "Aumente a sua presença online e alcance mais clientes com um site profissional feito à sua medida, criado por Rodrigo Freitas.",
 };
